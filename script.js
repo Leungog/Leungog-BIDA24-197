0301c18791992baf00485c85b65f30be647a8bd4
Leungog <bida24-197@thuto.bac.ac.bw>
2025-05-26 01:00:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function bookNow() {
+  window.location.href = "online-booking.html";
+}
+
+function submitBooking() {
+  alert("Your appointment has been submitted!");
+  return false; // Prevent real form submission
+}
