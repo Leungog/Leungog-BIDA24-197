1c2181e6b37f6bd871e229b0588e9cc70ef3200c
Leungog <bida24-197@thuto.bac.ac.bw>
2025-05-28 13:37:08 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ function submitBooking() {
   alert("Your appointment has been submitted!");
   return false; // Prevent real form submission
 }
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  document.getElementById("nav-links").classList.toggle("show");
+});
